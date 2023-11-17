384ed1f4526262d6850c405092d08821d35c5c9d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-17 10:48:50 -0800

    addded browser router in App.jsx and added Lost.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import HomeCarousel from './components/HomeCarousel.jsx'
+import React from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import HomeCarousel from './components/HomeCarousel.jsx';
+import Lost from './components/Lost.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <div id="root">
-        <Header />
-        <HomeCarousel />
-        <Footer />
+      <div id='root'>
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path='/' element={<HomeCarousel />}></Route>
+          </Routes>
+          <Routes>
+            <Route exact path='/lost' element={<Lost />}></Route>
+          </Routes>
+          <Footer />
+        </Router>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Lost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Lost() {
+  return (
+    <>Lost</>
+  )
+}
+
+export default Lost
