ad1f3ea62277e7631ea8e5601f6af6616e370561
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-17 14:30:04 -0800

    updated Item Card styling

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+import './ItemCard.css';
 
 function ItemCard() {
   return (
     <>
-      <Container className='item-card-component'>
-        <Image src='https://placehold.co/300x200' />
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className='mb-2 text-muted'>
-              Card Subtitle
-            </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Card.Link href='#'>Card Link</Card.Link>
-            <Card.Link href='#'>Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+      <Container id='item-card-container'>
+        <Image id='item-card-image' src='https://placehold.co/300x200' />
+        <div className='item-card-description'>
+          <h2>Item Name: Wallet</h2>
+          <p>Location: 123 Street, Seattle WA 98101</p>
+          <p>Keywords: black leather credit card cash license</p>
+        </div>
       </Container>
     </>
   );
