f4631d066bb0228ef1c047763bd5c8ddee768629
David Danilchik <davdd@mac.com>
2023-11-17 11:53:24 -0800

    started to set up auth0. Ready for backend implementation

---
+++ src/Auth/AuthButtons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import Login from "./Login.jsx";
+import Logout from "./Logout.jsx";
+
+function AuthButtons() {
+
+  const {
+    isAuthenticated,
+  } = useAuth0();
+
+  return isAuthenticated ? <Logout /> : <Login />
+}
+
+export default AuthButtons;
+
