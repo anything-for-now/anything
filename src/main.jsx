d503f3b591b5cf2d83aa1151b9c4de35eec0f194
David Danilchik <davdd@mac.com>
2023-11-18 13:48:47 -0800

    fixed mainjsx file to properly work with Auth0 params. Auth0 should work now on frontend.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
+
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
+const AUTH_CID = import.meta.env.VITE_AUTH_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Auth0Provider
+    domain= { AUTH_DOMAIN }
+    clientId= { AUTH_CID }
+    authorizeParams={{
+      redirect_uri: window.location.origin,
+    }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
 )
