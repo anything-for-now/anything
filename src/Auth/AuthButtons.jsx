d5a0eb22530b7eff480b21e4966bb8c6da51b47b
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-11-21 13:23:47 -0800

    auth for google maps

--- src/Auth/AuthButtons.jsx
+++ src/Auth/AuthButtons.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Login from "./Login.jsx";
-import { useAuthRequest } from './Authorization.js'; 
 
 function AuthButtons() {
   const { isAuthenticated } = useAuth0();
-  const { createUser } = useAuthRequest();
 
-  return !isAuthenticated ? <Login onCreateUser={createUser} /> : null;
+  return !isAuthenticated ? <Login /> : null;
 }
 
 export default AuthButtons;
