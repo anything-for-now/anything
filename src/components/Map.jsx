0b01ff7bdfc469ec1121f513f597c914d4225ca9
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-11-20 19:06:03 -0800

    added custom markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,74 +1,89 @@
 import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, OverlayView } from '@react-google-maps/api';
 import { Button } from 'react-bootstrap';
 import './Map.css';
-
-// const mapContainerStyle = {
-//   height: '500px',
-//   width: '600px',
-// };
 
 const defaultCenter = {
   lat: 47.608013,
   lng: -122.3328,
 };
 
-const Map = ({handleAddLocation, handleClose}) => {
+const Map = ({ handleAddLocation, handleClose, itemType }) => {
   const [mapCenter, setMapCenter] = useState(defaultCenter);
-  const [marker, setMarker] = useState(null);
+  const [items, setItems] = useState([]); 
   const [address, setAddress] = useState('');
-  const [tempMarker, setTempMarker] = useState(null);
+  const [tempMarker, setTempMarker] = useState(null); 
 
   const handleMapClick = (event) => {
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
+      type: itemType, 
     };
 
     setTempMarker(newMarker);
 
-    // Reverse geocoding to get the address for the clicked location
-    const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode({ location: newMarker }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        setAddress(results[0].formatted_address);
+   // Reverse geocoding to get the address for the clicked location
+   const geocoder = new window.google.maps.Geocoder();
+   geocoder.geocode({ location: newMarker }, (results, status) => {
+     if (status === 'OK' && results[0]) {
+       setAddress(results[0].formatted_address); // Set address
+     } else {
+       console.error('Reverse geocoding failed due to: ' + status);
+     }
+   });
+ };
+
+ const handleAddMarker = () => {
+  const geocoder = new window.google.maps.Geocoder();
+  handleAddLocation(address);
+  if (address) {
+    // Geocode the address entered by the user
+    geocoder.geocode({ address: address }, (results, status) => {
+      if (status === 'OK') {
+        const location = results[0].geometry.location;
+        const newMarker = {
+          lat: location.lat(),
+          lng: location.lng(),
+        };
+        setMapCenter(newMarker);
+        setAddress('');
       } else {
-        console.error('Reverse geocoding failed due to: ' + status);
+        alert(
+          'Geocode was not successful for the following reason: ' + status
+        );
       }
     });
-  };
+  } else if (tempMarker) {
+    setMapCenter(tempMarker);
+    setTempMarker(null);
+  } else {
+    alert(
+      'Please enter an address or click on the map to select a location.'
+    );
+  }
+  handleClose();
+};
 
-  const handleAddMarker = () => {
-    const geocoder = new window.google.maps.Geocoder();
-    handleAddLocation(address);
-    if (address) {
-      // Geocode the address entered by the user
-      geocoder.geocode({ address: address }, (results, status) => {
-        if (status === 'OK') {
-          const location = results[0].geometry.location;
-          const newMarker = {
-            lat: location.lat(),
-            lng: location.lng(),
-          };
-          setMarker(newMarker);
-          setMapCenter(newMarker);
-          setAddress('');
-        } else {
-          alert(
-            'Geocode was not successful for the following reason: ' + status
-          );
-        }
-      });
-    } else if (tempMarker) {
-      setMarker(tempMarker);
-      setMapCenter(tempMarker);
-      setTempMarker(null);
-    } else {
-      alert(
-        'Please enter an address or click on the map to select a location.'
-      );
-    }
-    handleClose();
+  const CustomMarker = ({ item }) => {
+    const getPixelPositionOffset = (width, height) => ({
+      x: -(width / 2),
+      y: -(height / 2),
+    });
+
+    const markerIcon = item.type === 'lost' ? '🚩' : '🕵️‍♂️'; 
+
+    return (
+      <OverlayView
+        position={{ lat: item.lat, lng: item.lng }}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        getPixelPositionOffset={getPixelPositionOffset}
+      >
+        <div className="custom-marker">
+          {markerIcon}
+        </div>
+      </OverlayView>
+    );
   };
 
   return (
@@ -80,15 +95,16 @@ const Map = ({handleAddLocation, handleClose}) => {
           zoom={8}
           onClick={handleMapClick}
         >
-          {marker && <Marker position={marker} />}
-          {tempMarker && <Marker position={tempMarker} />}
+          {items.map((item, idx) => (
+            <CustomMarker key={idx} item={item} />
+          ))}
+          {tempMarker && <CustomMarker item={tempMarker} />} 
         </GoogleMap>
       </div>
       <Button id='location-button' variant='primary' onClick={handleAddMarker}>
         Add Location
       </Button>
-      </>
-
+    </>
   );
 };
 
--- src/components/MapModal.jsx
+++ src/components/MapModal.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import Map from './Map';
 
-function MapModal({ show, handleClose, handleAddLocation }) {
+function MapModal({ show, handleClose, handleAddLocation, itemType }) {
   return (
     <Modal size='lg' show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Choose a location</Modal.Title>
       </Modal.Header>
-      <Map handleAddLocation={handleAddLocation} handleClose={handleClose}/>
+      <Map handleAddLocation={handleAddLocation} handleClose={handleClose} itemType={itemType} />
     </Modal>
   );
 }
 
-export default MapModal
+export default MapModal;
