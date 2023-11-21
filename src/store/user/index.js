b1c8fecd33bfd28c3df30ad5921a4b537382ddba
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-11-21 12:06:35 -0800

    resolving conflicts

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,17 +1,14 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
 const SERVER_URL = import.meta.env.SERVER_URL || 'http://localhost:3001';
 
 // Async thunk for fetching user profile
-export const fetchUserProfile = createAsyncThunk('user/fetchProfile', async (userId) => {
-  const response = await axios.get(`${SERVER_URL}/users/${userId}`);
+export const fetchUserProfile = createAsyncThunk('user/fetchProfile', async (auth0Id, { getState }) => {
+  const response = await axios.get(`${SERVER_URL}/user/${auth0Id}`);
   return response.data;
 });
 
 // Async thunk for updating user profile
-export const updateUserProfile = createAsyncThunk('user/updateProfile', async (userData) => {
-  const response = await axios.put(`${SERVER_URL}/users/update`, userData);
+export const updateUserProfile = createAsyncThunk('user/updateProfile', async (userData, { getState }) => {
+  const response = await axios.put(`${SERVER_URL}/user/update`, userData);
   return response.data;
 });
 
@@ -19,42 +16,20 @@ const userProfileSlice = createSlice({
   name: 'userProfile',
   initialState: {
     profileData: {},
-    loading: false,
-    error: null,
-    // other initial state properties...
   },
   reducers: {
-    // your reducers...
+    // Reducer methods, if needed
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchUserProfile.pending, (state) => {
-        state.loading = true;
-      })
       .addCase(fetchUserProfile.fulfilled, (state, action) => {
         state.profileData = action.payload;
-        state.loading = false;
-      })
-      .addCase(fetchUserProfile.rejected, (state, action) => {
-        state.error = action.error.message;
-        state.loading = false;
-      })
-      .addCase(updateUserProfile.pending, (state) => {
-        state.loading = true;
       })
       .addCase(updateUserProfile.fulfilled, (state, action) => {
         state.profileData = { ...state.profileData, ...action.payload };
-        state.loading = false;
-      })
-      .addCase(updateUserProfile.rejected, (state, action) => {
-        state.error = action.error.message;
-        state.loading = false;
       });
-      // add other cases as needed...
   },
 });
 
-// export any reducer actions if needed
-// export const { } = userProfileSlice.actions;
-
+export const { /* reducer actions */ } = userProfileSlice.actions;
 export default userProfileSlice.reducer;
